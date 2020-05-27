4f62f5b855e6349981096cecd5355d4ba001f2f5
Nan YE <dailystudio2010@gmail.com>
2020-05-27 16:06:39 +0800

    [*][DEV]: update scripts to support jcenter publish;

--- app/src/main/java/com/dailystudio/passport/sample/DigitalPassportSampleApplication.kt
+++ app/src/main/java/com/dailystudio/passport/sample/DigitalPassportSampleApplication.kt
@@ -1,10 +1,7 @@
 package com.dailystudio.passport.sample
 
 import com.dailystudio.devbricksx.app.DevBricksApplication
-import com.dailystudio.devbricksx.development.Logger
 import com.facebook.stetho.Stetho
-import com.nostra13.universalimageloader.core.ImageLoader
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
 
 class DigitalPassportSampleApplication : DevBricksApplication() {
 
